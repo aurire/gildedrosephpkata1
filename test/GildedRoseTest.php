b9aaf1519f90b340bf23d8ba235087d2f62bfc10
Rex von Aurum <frenkas@gmail.com>
2020-06-01 15:03:32 +0300

    KATA-01: some comments about what to do

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -3,10 +3,10 @@
 namespace App;
 
 class GildedRoseTest extends \PHPUnit\Framework\TestCase {
-    public function testFoo() {
+    public function testFooStaysFooAfterUpdate() {
         $items      = [new Item("foo", 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("fixme", $items[0]->name);
+        $this->assertEquals("foo", $items[0]->name);
     }
 }
